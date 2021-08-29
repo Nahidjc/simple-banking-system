2a0753dbb77bcf54b170b85d27d2d728a937827a
Nahidjc <nahidjc80@gmail.com>
2021-08-29 12:19:47 +0600

    dynamic adding navbr

--- banking/views.py
+++ banking/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
+
+
+def home(request):
+
+    return render(request, 'banking/home.html')
